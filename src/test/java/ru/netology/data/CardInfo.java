206ef2c81d444a78ee9614f7e3b846c2be9c849a
IlyaVatlin <107053785+IlyaVatlin@users.noreply.github.com>
2022-12-02 21:02:34 +0300

    Update CardInfo.java

--- src/test/java/ru/netology/data/CardInfo.java
+++ src/test/java/ru/netology/data/CardInfo.java
@@ -1,17 +1,14 @@
 package ru.netology.data;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
 @AllArgsConstructor
-@NoArgsConstructor
-public class CardInfo {
-        private String numberCard;
-        private String month;
-        private String year;
-        private String ownerCard;
-        private String cvc;
+@Data
 
+public class CardInfo {
+    private String cardNumber;
+    private String month;
+    private String year;
+    private String cardOwner;
+    private String CVC;
 }
